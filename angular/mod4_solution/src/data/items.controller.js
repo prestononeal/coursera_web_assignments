8b64cac956a9510e1ab3ae18142c032ab2abb77d
Preston O'Neal <prestononeal@gmail.com>
2016-11-26 22:41:33 -0800

    Split Categories and Items vertically

--- angular/mod4_solution/src/data/items.controller.js
+++ angular/mod4_solution/src/data/items.controller.js
@@ -8,6 +8,7 @@ ItemsController.$inject = ['items'];
 function ItemsController(items) {
   var itemsCtrl = this;
   itemsCtrl.items = items.data.menu_items;
+  itemsCtrl.category = items.data.category.name;
 }
 
 })();
